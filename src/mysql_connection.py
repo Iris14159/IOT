7949d1ad2f04e1bac78973df8f4e4b7fd6ee9366
Iris14159 <ampheriz0@gmail.com>
2023-10-30 00:45:57 -0600

    Connection

--- src/mysql_connection.py
+++ src/mysql_connection.py
@@ -10,5 +10,15 @@ try:
     )
     if connection.is_connected():
         print("Connected.")
+        info_server = connection.get_server_info()
+        print(info_server)
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATABASE()")
+        row = cursor.fetchone()
+        print("Data base: {}".format(row))
 except Exception as ex:
     print(Ex)
+finally:
+    if connection.is_connected():
+        connection.close()
+        print("Connection finished.")
